d9d46b084e28e00e537bde0961c331ac39281f7e
Vladimir Lukyanov <vladimir@liikyanov.com>
2022-07-05 10:05:51 +0300

    v1.0.6917 – examples resizable update

--- docs/assets/js/code-example.js
+++ docs/assets/js/code-example.js
@@ -16,35 +16,56 @@
 
     // Resizable
 
-    const initDragHandle = (resizer, preview) => {
+    const initDragHandle = (event, resizer, preview, limit) => {
+        let position = event.clientX;
+        let margin = parseInt(preview.style.marginRight, 10);
+        document.documentElement.classList.add('dragging')
 
         document.onmousemove = (event) => {
-            return eventDragHandler(event, resizer, preview);
+            return eventDragHandler(event, resizer, preview, position, margin, limit);
         }
 
         document.onmouseup = () => {
             document.onmousemove = null;
             document.onmouseup = null;
+            document.documentElement.classList.remove('dragging')
         }
     }
 
-    const eventDragHandler = (event, resizer, preview) => {
-        preview.style.width = event.clientX + 'px';
+    const eventDragHandler = (event, resizer, preview, position, margin, limit) => {
+
+        let offset = margin + position - event.clientX;
+
+        if (offset > 0) {
+            if (offset < limit) {
+                preview.style.marginRight = offset + 'px';
+            } else {
+                preview.style.marginRight = limit + 'px';
+            }
+        } else {
+            preview.style.marginRight = '0px';
+        }
     }
 
-    // Wrap programmatically code blocks and add copy btn.
-    document.querySelectorAll('.code-example')
-        .forEach(element => {
-            let preview = element.querySelector('.example-preview');
-            if (preview) { // Ignore examples made be shortcode
+    window.onload = () => {
+        // Wrap programmatically code blocks and add copy btn.
+        document.querySelectorAll('.code-example')
+            .forEach(element => {
+                let preview = element.querySelector('.example-preview');
 
-                let resizer = element.querySelector('.example-preview .resizer');
+                if (preview) { // Ignore examples made be shortcode
 
-                if (resizer) {
-                    resizer.onmousedown = () => {
-                        return initDragHandle(resizer, preview)
+                    let resizer = element.querySelector('.example-preview .resizer .drag');
+
+                    if (resizer) {
+                        let container_limit = preview.clientWidth / 2;
+                        // console.log(preview.style, container_limit);
+                        resizer.onmousedown = (event) => {
+                            return initDragHandle(event, resizer, preview, container_limit)
+                        }
                     }
                 }
-            }
-        })
+            })
+    }
+
 })()
